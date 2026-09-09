ebfabf7a2879723ba444996d43e8d13e26f7d99a
bira10a <94951002+bira10a@users.noreply.github.com>
2026-09-09 14:22:54 +0300

    added Constructor newAccount

--- main.go
+++ main.go
@@ -25,6 +25,14 @@ func (acc *account) generatePassword(n int) {
 	acc.password = string(res)
 }
 
+func newAccount(login, password, url string) *account {
+	return &account{
+		login:    login,
+		password: password,
+		url:      url,
+	}
+}
+
 var letterRunes = []rune("1234567890qwertyuiopQWERTYUIOP")
 
 func main() {
@@ -32,11 +40,7 @@ func main() {
 	password := promptData("Введите password: ")
 	url := promptData("Введите url: ")
 
-	myAccount := account{
-		login:    login,
-		password: password,
-		url:      url,
-	}
+	myAccount := newAccount(login, password, url)
 
 	myAccount.generatePassword(10)
 	myAccount.outputPassword()
