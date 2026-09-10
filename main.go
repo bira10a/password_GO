052fcb1a7d2014dad976160df15cf2f54b1d1593
bira10a <94951002+bira10a@users.noreply.github.com>
2026-09-10 11:21:11 +0300

    добавил немного валидации в конструктор

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
+	"net/url"
 )
 
 type account struct {
@@ -25,12 +27,23 @@ func (acc *account) generatePassword(n int) {
 	acc.password = string(res)
 }
 
-func newAccount(login, password, url string) *account {
-	return &account{
+func newAccount(login, password, urlString string) (*account, error) {
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+
+	newAccount := &account{
 		login:    login,
 		password: password,
-		url:      url,
+		url:      urlString,
 	}
+
+	if login == "" {
+		newAccount.generatePassword(10)
+	}
+
+	return newAccount, nil
 }
 
 var letterRunes = []rune("1234567890qwertyuiopQWERTYUIOP")
@@ -40,7 +53,11 @@ func main() {
 	password := promptData("Введите password: ")
 	url := promptData("Введите url: ")
 
-	myAccount := newAccount(login, password, url)
+	myAccount, err := newAccount(login, password, url)
+	if err != nil {
+		fmt.Println("Кривой у Вас URL")
+		return
+	}
 
 	myAccount.generatePassword(10)
 	myAccount.outputPassword()
@@ -49,6 +66,6 @@ func main() {
 func promptData(prompt string) string {
 	fmt.Print(prompt)
 	var res string
-	fmt.Scan(&res)
+	fmt.Scanln(&res)
 	return res
 }
